5752c1738fdeb9730af14c7e51a349ed863b626e
Jalicewm <janebaldwintolliver@yahoo.com>
2023-03-06 21:16:56 -0600

    Updating every second
    Date and time update every 1000ms using setInterval()

--- javascript/index.js
+++ javascript/index.js
@@ -1,40 +1,54 @@
-let washingtonElement = document.querySelector("#washington");
-let washingtonDateElement = washingtonElement.querySelector(".date");
-let washingtonTimeElement = washingtonElement.querySelector(".time");
-let washingtonTime = moment().tz("America/New_York");
-washingtonDateElement.innerHTML = washingtonTime.format("MMMM	Do YYYY");
-washingtonTimeElement.innerHTML = washingtonTime.format(
-  "h:mm:ss [<small>]A[</small>]"
-);
+setInterval(function () {
+  let washingtonElement = document.querySelector("#washington");
+  let washingtonDateElement = washingtonElement.querySelector(".date");
+  let washingtonTimeElement = washingtonElement.querySelector(".time");
+  let washingtonTime = moment().tz("America/New_York");
+  washingtonDateElement.innerHTML = washingtonTime.format("MMMM Do YYYY");
+  washingtonTimeElement.innerHTML = washingtonTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}, 1000);
 
-let caracasElement = document.querySelector("#caracas");
-let caracasDateElement = caracasElement.querySelector(".date");
-let caracasTimeElement = caracasElement.querySelector(".time");
-let caracasTime = moment().tz("America/Caracas");
-caracasDateElement.innerHTML = caracasTime.format("MMMM	Do YYYY");
-caracasTimeElement.innerHTML = caracasTime.format(
-  "h:mm:ss [<small>]A[</small>]"
-);
+setInterval(function () {
+  let caracasElement = document.querySelector("#caracas");
+  let caracasDateElement = caracasElement.querySelector(".date");
+  let caracasTimeElement = caracasElement.querySelector(".time");
+  let caracasTime = moment().tz("America/Caracas");
+  caracasDateElement.innerHTML = caracasTime.format("MMMM	Do YYYY");
+  caracasTimeElement.innerHTML = caracasTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}, 1000);
 
-let madridElement = document.querySelector("#madrid");
-let madridDateElement = madridElement.querySelector(".date");
-let madridTimeElement = madridElement.querySelector(".time");
-let madridTime = moment().tz("Europe/Madrid");
-madridDateElement.innerHTML = madridTime.format("MMMM	Do YYYY");
-madridTimeElement.innerHTML = madridTime.format("h:mm:ss [<small>]A[</small>]");
+setInterval(function () {
+  let madridElement = document.querySelector("#madrid");
+  let madridDateElement = madridElement.querySelector(".date");
+  let madridTimeElement = madridElement.querySelector(".time");
+  let madridTime = moment().tz("Europe/Madrid");
+  madridDateElement.innerHTML = madridTime.format("MMMM Do YYYY");
+  madridTimeElement.innerHTML = madridTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}, 1000);
 
-let guatemalaElement = document.querySelector("#guatemala");
-let guatemalaDateElement = guatemalaElement.querySelector(".date");
-let guatemalaTimeElement = guatemalaElement.querySelector(".time");
-let guatemalaTime = moment().tz("America/Guatemala");
-guatemalaDateElement.innerHTML = guatemalaTime.format("MMMM	Do YYYY");
-guatemalaTimeElement.innerHTML = guatemalaTime.format(
-  "h:mm:ss [<small>]A[</small>]"
-);
+setInterval(function () {
+  let guatemalaElement = document.querySelector("#guatemala");
+  let guatemalaDateElement = guatemalaElement.querySelector(".date");
+  let guatemalaTimeElement = guatemalaElement.querySelector(".time");
+  let guatemalaTime = moment().tz("America/Guatemala");
+  guatemalaDateElement.innerHTML = guatemalaTime.format("MMMM	Do YYYY");
+  guatemalaTimeElement.innerHTML = guatemalaTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}, 1000);
 
-let mexicoElement = document.querySelector("#mexico");
-let mexicoDateElement = mexicoElement.querySelector(".date");
-let mexicoTimeElement = mexicoElement.querySelector(".time");
-let mexicoTime = moment().tz("America/Mexico_City");
-mexicoDateElement.innerHTML = mexicoTime.format("MMMM	Do YYYY");
-mexicoTimeElement.innerHTML = mexicoTime.format("h:mm:ss [<small>]A[</small>]");
+setInterval(function () {
+  let mexicoElement = document.querySelector("#mexico");
+  let mexicoDateElement = mexicoElement.querySelector(".date");
+  let mexicoTimeElement = mexicoElement.querySelector(".time");
+  let mexicoTime = moment().tz("America/Mexico_City");
+  mexicoDateElement.innerHTML = mexicoTime.format("MMMM Do YYYY");
+  mexicoTimeElement.innerHTML = mexicoTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}, 1000);
